9017682ad510d7381b39027b4fe546967931a4a8
danieldmonteiro <danieldmonteiro@gmail.com>
2024-11-14 23:10:41 -0300

    segundo commit

--- exercicio_1/exercicio.py
+++ exercicio_1/exercicio.py
@@ -1,7 +1,56 @@
-print("Olá, mundo!")
-soma = 10 + 5
-print(soma)
-a = input("Digite o primeiro valor: ")
-b = input("Digite o segundo valor: ")
-soma = float(a) + float(b)
-print(soma)
+print()
+
+#Exercício 1.1
+
+print("Hello, world!")
+
+#Espaço entre exercícios
+
+print()
+
+#Exercício 1.2.
+
+print("Extremamente básico\n")
+A = input("Digite o primeiro número inteiro: ")
+B = input("Digite o segundo número inteiro: ")
+X = int(A) + int(B)
+print(f"X =  {X}")
+
+#Espaço entre exercícios
+
+print()
+
+#Exercício 1.3.
+print("Área do círculo\n")
+raio = float(input("Digite o raio do círculo: "))
+A = 3.14159 * (raio ** 2)
+print(f"A área do círculo é {A}")
+
+#Espaço entre exercícios
+
+print()
+
+#Exercício 1.4.
+print("Soma simples\n")
+A = input("Digite o primeiro número inteiro: ")
+B = input("Digite o segundo número inteiro: ")
+S = int(A) + int(B)
+print(f"SOMA = {S}")
+
+#Espaço entre exercícios
+
+print()
+
+#Exercício 1.5.
+
+print("Produto simples\n")
+A = input("Digite o primeiro número inteiro: ")
+B = input("Digite o segundo número inteiro: ")
+PROD = int(A) * int(B)
+print(f"PROD = {PROD}")
+
+#Espaço entre exercícios
+
+print()
+
+print("FIM!\n")
